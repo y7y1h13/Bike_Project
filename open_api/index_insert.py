9b7655711223ba0daeebed8f15146c4a53154b3e
HYUNMIN YOO <2049905@naver.com>
2022-06-20 14:49:14 +0900

    sql_insert

---
+++ open_api/index_insert.py
@@ -0,0 +1,38 @@
+import json
+import requests
+import pymysql
+from sqlalchemy import text, create_engine
+
+
+engine = create_engine('mysql+pymysql://bike:dbgusals1@localhost:3306/use_bike')
+
+for i in range(1, 2002, 1000):
+
+    if i == 2001:
+        url = f'http://openapi.seoul.go.kr:8088/707950714679377934386e64746352/json/bikeListHist/{2001}/{2872}/20220620'
+
+    else:
+        url = f'http://openapi.seoul.go.kr:8088/707950714679377934386e64746352/json/bikeListHist/{i}/{i + 999}/20220620'
+
+    response = requests.get(url)
+    json_ob = json.loads(response.content)
+    json_ar = json_ob.get('getStationListHist')
+    json_ar1 = json_ar.get('row')
+
+    sql = """
+            insert into id_index(stationId, stationName)
+            values(
+            :stationId,
+            :stationName
+            )
+            """
+
+    for i in json_ar1:
+        id = i.get('stationId')
+        name = i.get('stationName')
+        if name[0].isdigit():
+            name = name[5:].lstrip().rstrip()
+        dt = {"stationId": id,
+              "stationName": name}
+        engine.execute(text(sql), **dt)
+
--- open_api/seoul_bike.py
+++ open_api/seoul_bike.py
@@ -2,30 +2,54 @@ import json
 import requests
 import pymysql
 from sqlalchemy import text, create_engine
-from datetime import datetime
+from datetime import date, timedelta
 
 engine = create_engine('mysql+pymysql://bike:dbgusals1@localhost:3306/use_bike')
 
-for i in range(1, 2002, 1000):
+for h in range(24):
+    for i in range(1, 2002, 1000):
+        today = date.today()
+        yesterday = date.today() - timedelta(1)
 
-    url = f'http://openapi.seoul.go.kr:8088/707950714679377934386e64746352/json/bikeList/{i}/{i + 999}/{datetime.today().strftime("%Y%m%d")}'
+        if i == 2001:
+            url = f'http://openapi.seoul.go.kr:8088/707950714679377934386e64746352/json/bikeListHist/{2001}/{2872}/{yesterday.strftime("%Y%m%d")}{h}'
 
-    response = requests.get(url)
-    json_ob = json.loads(response.content)
-    json_ar = json_ob.get('rentBikeStatus')
-    json_ar1 = json_ar.get('row')
+        else:
+            url = f'http://openapi.seoul.go.kr:8088/707950714679377934386e64746352/json/bikeListHist/{i}/{i + 999}/{yesterday.strftime("%Y%m%d")}{h}'
 
-    sql = """
-    insert into bike(stationName, parking_Bike, shared)
-    values(
-    :station,
-    :parking,
-    :shared)
-    """
+        response = requests.get(url)
+        json_ob = json.loads(response.content)
+        json_ar = json_ob.get('getStationListHist')
+        json_ar1 = json_ar.get('row')
 
-    for i in json_ar1:
-        station = i.get('stationName')
-        parking = i.get('parkingBikeTotCnt')
-        share = i.get('shared')
-        dt = {"station": station[5:].lstrip().rstrip(), "parking": parking, 'shared': share}
-        engine.execute(text(sql), **dt)
+        sql = """
+        insert into bike(Date, stationId, stationName, rackTotCnt, parkingBike, shared, stationLatitude, stationLongitude)
+        values(
+        :Date,
+        :stationId,
+        :stationName,
+        :rackTotCnt,
+        :parkingBike,
+        :shared,
+        :stationLatitude,
+        :stationLongitude
+        )
+        """
+
+        for i in json_ar1:
+            stationName = i.get('stationName')
+            stationId = i.get('stationId')
+            parkingBike = i.get('parkingBikeTotCnt')
+            shared = i.get('shared')
+            rackTotCnt = i.get('rackTotCnt')
+            stationLatitude = i.get('stationLatitude')
+            stationLongitude = i.get('stationLongitude')
+
+            if stationName[0].isdigit():
+                stationName = stationName[5:].lstrip().rstrip()
+
+            dt = {"Date": f'{yesterday.strftime("%Y-%m-%d")}" "{h}', "stationId": stationId,
+                  "stationName": stationName,
+                  "parkingBike": parkingBike, 'shared': shared, 'rackTotCnt': rackTotCnt,
+                  'stationLatitude': stationLatitude, 'stationLongitude': stationLongitude}
+            engine.execute(text(sql), **dt)
