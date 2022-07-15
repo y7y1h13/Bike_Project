86c7ca10bc897309f37d7cfd1f548e3e0b256fbe
YOO <2049905@naver.com>
2022-07-15 14:59:48 +0900

    db_insert

--- db_insert/bike_db_insert/bulk_insert.py
+++ db_insert/bike_db_insert/bulk_insert.py
@@ -13,43 +13,43 @@ engine = create_engine('mysql+pymysql://bike:dbgusals1@etl.cgskizjipfsf.ap-north
 
 
 try:
-    for h in range(now.hour + 1, 24):
-        for i in range(1, 2002, 1000):
-            dic = defaultdict(list)
-            today = date.today()
-            yesterday = date.today() - timedelta(1)
+    h = now.hour + 1
+    for i in range(1, 2002, 1000):
+        dic = defaultdict(list)
+        today = date.today()
+        yesterday = date.today() - timedelta(1)
 
-            if i == 2001:
-                url = f'http://openapi.seoul.go.kr:8088/707950714679377934386e64746352/json/bikeListHist/{2001}/{2872}/{yesterday.strftime("%Y%m%d")}{h}'
+        if i == 2001:
+            url = f'http://openapi.seoul.go.kr:8088/707950714679377934386e64746352/json/bikeListHist/{2001}/{2872}/{yesterday.strftime("%Y%m%d")}{h}'
 
+        else:
+            url = f'http://openapi.seoul.go.kr:8088/707950714679377934386e64746352/json/bikeListHist/{i}/{i + 999}/{yesterday.strftime("%Y%m%d")}{h}'
+
+
+
+
+        response = requests.get(url)
+        json_ob = json.loads(response.content)
+        json_ar = json_ob.get('getStationListHist')
+        json_ar1 = json_ar.get('row')
+
+        for j in json_ar1:
+            stationName = j.get('stationName')
+            if stationName[0].isdigit():
+                stationName = stationName[5:].lstrip().rstrip()
+            if h < 10:
+                dic['Date'].append(f'{yesterday.strftime("%Y-%m-%d")} 0{h}')
             else:
-                url = f'http://openapi.seoul.go.kr:8088/707950714679377934386e64746352/json/bikeListHist/{i}/{i + 999}/{yesterday.strftime("%Y%m%d")}{h}'
+                dic['Date'].append(f'{yesterday.strftime("%Y-%m-%d")} {h}')
+            dic['stationId'].append((j.get('stationId'))[3:])
+            dic['stationName'].append(stationName)
+            dic['parkingBike'].append(j.get('parkingBikeTotCnt'))
+            dic['shared'].append(float(j.get('shared')) * 0.01)
+            dic['rackTotCnt'].append(j.get('rackTotCnt'))
+            dic['location'].append(f"{j.get('stationLatitude')},{j.get('stationLongitude')}")
 
-
-
-
-            response = requests.get(url)
-            json_ob = json.loads(response.content)
-            json_ar = json_ob.get('getStationListHist')
-            json_ar1 = json_ar.get('row')
-
-            for j in json_ar1:
-                stationName = j.get('stationName')
-                if stationName[0].isdigit():
-                    stationName = stationName[5:].lstrip().rstrip()
-                if h < 10:
-                    dic['Date'].append(f'{yesterday.strftime("%Y-%m-%d")} 0{h}')
-                else:
-                    dic['Date'].append(f'{yesterday.strftime("%Y-%m-%d")} {h}')
-                dic['stationId'].append((j.get('stationId'))[3:])
-                dic['stationName'].append(stationName)
-                dic['parkingBike'].append(j.get('parkingBikeTotCnt'))
-                dic['shared'].append(float(j.get('shared')) * 0.01)
-                dic['rackTotCnt'].append(j.get('rackTotCnt'))
-                dic['location'].append(f"{j.get('stationLatitude')},{j.get('stationLongitude')}")
-
-            df = pd.DataFrame(dic)
-            df.to_sql(name='bike', con=engine, if_exists='append', index=False)
+        df = pd.DataFrame(dic)
+        df.to_sql(name='bike', con=engine, if_exists='append', index=False)
     end = time.time()
     print(f"{end - start:.5f} sec")
 except:
